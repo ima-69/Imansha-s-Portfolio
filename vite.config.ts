2f26625aff7599757eca5ae42309a00f23ddde22
ima-69 <imansha.idr@gmail.com>
2025-05-22 00:04:20 +0530

    add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/Imansha-s-Portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
 });
